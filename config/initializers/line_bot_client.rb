c70506b897e902144e899f1672699b6e86f38a09
佐藤有斗 <satoyuito@satoyuito-MacBook-Pro.local>
2017-02-15 21:03:10 +0900

    Fux line_bot_client.rb

--- config/initializers/line_bot_client.rb
+++ config/initializers/line_bot_client.rb
@@ -1,6 +1,6 @@
 require 'line/bot'
 
-Line::Bot::Client.new { |config|
+Line::Bot::Client.configure { |config|
   config.channel_secret = ENV['LINE_CHANNEL_SECRET']
   config.channel_token = ENV['LINE_CHANNEL_TOKEN']
 }
