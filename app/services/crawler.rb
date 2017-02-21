5264034c246a042f6e2cb0256dda0ea81990af2b
佐藤有斗 <satoyuito@satoyuito-MacBook-Pro.local>
2017-02-21 15:43:22 +0900

    add transaction

--- app/services/crawler.rb
+++ app/services/crawler.rb
@@ -1,14 +1,16 @@
 class Crawler
-  def execute
+  def execute!
     Settings.sites.each do |site|
       feed = Feedjira::Feed.fetch_and_parse(site)
-      feed.entries.each do |entry|
-        Article.where(url: entry.url).first_or_create(
-          title:        entry.title,
-          url:          entry.url,
-          content:      entry.content,
-          published_at: entry.published
-        )
+      Article.transaction do
+        feed.entries.each do |entry|
+          Article.where(url: entry.url).first_or_create!(
+            title:        entry.title,
+            url:          entry.url,
+            content:      entry.content,
+            published_at: entry.published
+          )
+        end
       end
     end
   end
