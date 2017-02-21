2e5754a70a88ad480cc433f5d1fbbea759d2a5de
佐藤有斗 <satoyuito@satoyuito-MacBook-Pro.local>
2017-02-21 20:31:42 +0900

    add association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ApplicationRecord
+  has_many :keyphrases, foreign_key: :content_id
 end
--- app/models/keyphrase.rb
+++ app/models/keyphrase.rb
@@ -1,2 +1,3 @@
 class Keyphrase < ApplicationRecord
+  belongs_to :article, class_name: :Article, foreign_key: :content_id
 end
